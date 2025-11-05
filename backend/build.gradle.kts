1a4ca5bc2c7c320e26b3d80b69a82403edfabd66
prayanne <kyhy0321@gmail.com>
2025-11-06 04:03:58 +0900

    add java_mainclass and edit build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,31 +1,27 @@
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
-
 plugins {
-  id("org.springframework.boot") version "3.3.4"
-  id("io.spring.dependency-management") version "1.1.6"
-  kotlin("jvm") version "2.0.10" apply false // if not use kotlin, U can remove this section
-  java
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+    java
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_21
+
+java {
+    toolchain { languageVersion.set(JavaLanguageVersion.of(21)) }
+}
 
 repositories { mavenCentral() }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  runtimeOnly("org.postgresql:postgresql:42.7.4")
-
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("org.postgresql:postgresql:42.7.4")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<Test>{
-  useJUnitPlatform()
+// 메인클래스 지정
+springBoot {
+    mainClass.set("com.example.demo.DemoApplication")
 }
+
