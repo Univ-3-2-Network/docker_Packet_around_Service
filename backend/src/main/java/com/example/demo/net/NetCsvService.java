0bea5bd0f03de5e588e38dfe875eb678a25f9576
prayanne <kyhy0321@gmail.com>
2025-11-07 01:32:07 +0900

    editing function_part / extract data

--- backend/src/main/java/com/example/demo/net/NetCsvService.java
+++ backend/src/main/java/com/example/demo/net/NetCsvService.java
@@ -37,8 +37,56 @@ public class NetCsvService{
         e.setInfo(info);
 
         // pattern Parsing: TCP flags || HTTP || DNS
-        
+        String lower = info == null ? "" : info.toLowerCase(Locale.ROOT);
+
+        // if protocol == tcp / parsing flags
+        if("tcp".equalsIgnoreCase(proto)){
+          String flags = extractTcpFlags(info);
+          e.setTcpFlags(flags);
+        }
+
+        // if protocol == http / parsing path, HTTP ver, answerCode, Host, e.t.c.
+        if("http".equalsIgnoreCase(proto)){
+
+          if (lower.startWith("get ")) { e.setHttpMethod("GET"); e.setHttpPath(extractHttpPath(info)); }
+          else if(lower.startWith("post ")) { e.setHttpMethod("POST"); e.setHttpPath(extractHttpPath(info)); }
+          else if(lower.startWith("http/")) {e.setHttpStatus(extractHttpStatus(info)); }
+          if (lower.startWith("host:")) { e.setHttpHost(info.substring(5).trim()); }
+        }
+
+        // if protocol == dns / parsing standard query, ns name || query resp, e.t.c.
+        if("dns".equalsIgnoreCase(proto)){
+          
+          if (lower.contains("standard query")){ e.setDnsQuery(extractDnsQuery(info)); }
+          if (lower.contains("response")) { e.setDnsAnswer(extractDnsAnswer(info)); }
+        }
+
+        repo.save(e);
+        count++;
       }
+      return count;
     }
+  }
+
+  private static String get(CSVRecord r, String key) { return r.isMapped(key) ? r.get(key) : null; }
+  private static Integer parseInt(String s) {
+    try { return s == null? null: Integer.parseInt(s.trim()); }
+    catch(Exception e) {return null;}
+  }
+  private static Double(String s) {
+    try { return s == null? null: Double.parseDouble(s.trim()); }
+    catch(Exception e) { return null; }
+  }
+
+  // TCP / extract Flags 
+  private static String extractTcpFlags(String info) {
+    if (info == null) { return null; }
+    int i = info.indexOf('['), j = info.indexOf(']');
+    return (i>=0 && j >i) ? info.substring(i+1, j) : null;
+  }
+
+  private static String extractHttpPath(String info){
+    if (info == null ) {return null; }
+    String[] parts = info.split(" ");
   }
 }
