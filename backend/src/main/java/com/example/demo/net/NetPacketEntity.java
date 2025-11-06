2a0c0899c9f4d5a004301070922f85450e656a33
prayanne <kyhy0321@gmail.com>
2025-11-07 02:04:16 +0900

    add Entity's setter getter

--- backend/src/main/java/com/example/demo/net/NetPacketEntity.java
+++ backend/src/main/java/com/example/demo/net/NetPacketEntity.java
@@ -27,4 +27,29 @@ public class NetPacketEntity{
   private String dnsAnswer;
 
   // TODO: need add getter, setter parts
+  public Double  setTimeRel() { return timeRel; }
+  public String  setSrc() { return src; }
+  public String  setDst() { return dst; }
+  public String  setProtocol() { return protocol; }
+  public Integer setLength() { return length; }
+  public String  setTcpFlags() { return tcpFlags; }
+  public String  setHttpMethod() { return httpMethod; }
+  public String  setHttpHost() { return httpHost; }
+  public String  setHttpPath() { return httpPath; }
+  public Integer setHttpStatus() { return httpStatus; }
+  public String  setDnsQuery() { return dnsQuery; }
+  public String  setDnsAnswer() { return dnsAnswer; }
+
+  public Double  getTimeRel() { return timeRel; }
+  public String  getSrc() { return src; }
+  public String  getDst() { return dst; }
+  public String  getProtocol() { return protocol; }
+  public Integer getLength() { return length; }
+  public String  getTcpFlags() { return tcpFlags; }
+  public String  getHttpMethod() { return httpMethod; }
+  public String  getHttpHost() { return httpHost; }
+  public String  getHttpPath() { return httpPath; }
+  public Integer getHttpStatus() { return httpStatus; }
+  public String  getDnsQuery() { return dnsQuery; }
+  public String  getDnsAnswer() { return dnsAnswer; }
 }
