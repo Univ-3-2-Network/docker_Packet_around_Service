a613cef8cfb5e66cfd3dac832776288741da07f7
prayanne <kyhy0321@gmail.com>
2025-11-09 22:35:45 +0900

    edit typo err / jdb -> jdbc

--- backend/src/main/java/com/example/demo/net/NetController.java
+++ backend/src/main/java/com/example/demo/net/NetController.java
@@ -29,7 +29,7 @@ public class NetController {
     @GetMapping("/latest")
     public List<Map<String,Object>> latest(@RequestParam(defaultValue="50") int limit){
         limit = Math.max(Math.min(500, limit));
-        return jdb.queryForList("SELECT ts_utc, src, dst, protocol, length, info FROM net_packets DESC LIMIT ?", limit);
+        return jdbc.queryForList("SELECT ts_utc, src, dst, protocol, length, info FROM net_packets DESC LIMIT ?", limit);
     } 
 
     // Analysis DNS query || simple ver
